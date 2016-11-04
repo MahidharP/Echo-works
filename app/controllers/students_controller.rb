58fbd534d77c7056da41a850290e80e2741e88a2
Mahi Reddy <mahidharreddy10@gmail.com>
2016-11-04 18:41:02 +0530

    Changed in controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -72,6 +72,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:name, :email, :mobile, :collge, :beneficial, :feedback, :join, :consult, :suggestion, :college_id, :workshop_id, :technology_ids)
+      params.require(:student).permit(:name, :email, :mobile, :collge, :beneficial, :feedback, :join, :consult, :suggestion, :college_id, :workshop_id, technology_ids: [])
     end
 end
