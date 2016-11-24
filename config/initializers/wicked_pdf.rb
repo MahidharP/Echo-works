f25c7b67f44c50a6727082b2db86064814660498
Mahi Reddy <mahidharreddy10@gmail.com>
2016-11-24 14:23:37 +0530

    small changes

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -3,9 +3,10 @@
 # }
 
 if Rails.env.production?
-  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+    wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
 else
-  wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"            
+    wkhtmltopdf_path = '/Users/p.mahidharreddy/.rbenv/shims/wkhtmltopdf'
+    # wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"
 end
 
 WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
