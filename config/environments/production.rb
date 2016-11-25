e4f33b65a171e295aa18c5ed64951fd3e06e14d3
Mahi Reddy <mahidharreddy10@gmail.com>
2016-11-25 16:19:50 +0530

    17-24

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
     # Full error reports are disabled and caching is turned on.
     config.consider_all_requests_local       = false
     config.action_controller.perform_caching = true
+    config.action_mailer.raise_delivery_errors = true
 
     config.middleware.use ExceptionNotification::Rack,
                           email: {
