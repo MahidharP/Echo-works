c0eb7477e157af682ae637441985b0ec24c81e9a
Mahi Reddy <mahidharreddy10@gmail.com>
2016-11-25 17:35:36 +0530

    17-24

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,17 +16,6 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_deliveries = true
     config.action_mailer.delivery_method = :smtp
-    ActionMailer::Base.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-        address: 'smtp.gmail.com',
-        port: 587,
-        domain: 'gmail.com',
-        authentication: 'plain',
-        enable_starttls_auto: true,
-        user_name: 'nanisty143@gmail.com',
-        password: 'mahidhar',
-        openssl_verify_mode: 'none'
-    }
 
     config.middleware.use ExceptionNotification::Rack,
                           email: {
