211caba0e991d033996fcce0417bf4d169f730de
Mahi Reddy <mahidharreddy10@gmail.com>
2016-11-25 11:26:47 +0530

    migrations

--- db/migrate/20161122061332_rename_column_collge_from_students.rb
+++ db/migrate/20161122061332_rename_column_collge_from_students.rb
@@ -1,7 +1,6 @@
 class RenameColumnCollgeFromStudents < ActiveRecord::Migration
-  def change
-    rename_column :students, :collge, :college
-    rename_column :students, :join, :joining
-    rename_column :students, :college, :college_code
-  end
+    def change
+        rename_column :students, :join, :joining
+        rename_column :students, :collge, :college_code
+    end
 end
