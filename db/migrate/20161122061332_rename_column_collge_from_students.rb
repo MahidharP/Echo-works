3b73559f11479b9195136338b309b1f0152aff29
Mahi Reddy <mahidharreddy10@gmail.com>
2016-11-25 12:45:33 +0530

    migrations

--- db/migrate/20161122061332_rename_column_collge_from_students.rb
+++ db/migrate/20161122061332_rename_column_collge_from_students.rb
@@ -1,6 +1,6 @@
 class RenameColumnCollgeFromStudents < ActiveRecord::Migration
     def change
-        remove_column :students, :join
+        rename_column :students, :join, :joining
         remove_column :students, :collge
     end
 end
--- db/migrate/20161125070223_add_college_code_to_students.rb
+++ db/migrate/20161125070223_add_college_code_to_students.rb
@@ -1,6 +1,5 @@
 class AddCollegeCodeToStudents < ActiveRecord::Migration
     def change
-        add_column :students, :joining, :boolean
         add_column :students, :college_code, :string
     end
 end
