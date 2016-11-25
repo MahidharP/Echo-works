dd9756d2e6349aaa7e0b66f0f63e08826f7c7f45
Mahi Reddy <mahidharreddy10@gmail.com>
2016-11-25 16:54:58 +0530

    17-24

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
 
     config.middleware.use ExceptionNotification::Rack,
                           email: {
+                              deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
+                              email_prefix: '[PREFIX] ',
                               sender_address: %("notifier" <nanisty143@gmail.com>),
                               exception_recipients: %w(mahidharreddy10@gmail.com)
                           }
