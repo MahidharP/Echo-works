4c04ca1b2727dd27df686aa8a2e6277991f80769
Mahi Reddy <mahidharreddy10@gmail.com>
2016-11-28 12:18:58 +0530

    minot

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_deliveries = true
     config.action_mailer.delivery_method = :smtp
+    ActionMailer::Base.delivery_method = :smtp
 
     config.middleware.use ExceptionNotification::Rack,
                           email: {
