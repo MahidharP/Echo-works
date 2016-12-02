cdb1a5874b7b94618083db9ef0b7b20529c4d0cf
Mahi Reddy <mahidharreddy10@gmail.com>
2016-12-02 12:43:25 +0530

    hi

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -3,7 +3,8 @@
 # }
 
 if Rails.env.production?
-    wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+    # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+    wkhtmltopdf_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
 else
     wkhtmltopdf_path = '/Users/p.mahidharreddy/.rbenv/shims/wkhtmltopdf'
     # wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"
