6b37812cd0802eff371962eeb0f9c3f2b2641aeb
Mahi Reddy <mahidharreddy10@gmail.com>
2016-12-02 12:58:03 +0530

    mm

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,13 +1,14 @@
-# WickedPdf.config = {
+ WickedPdf.config = {
 #   exe_path: '/Users/p.mahidharreddy/.rbenv/shims/wkhtmltopdf'
-# }
-
-if Rails.env.production?
-    # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
     wkhtmltopdf_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
-else
-    wkhtmltopdf_path = '/Users/p.mahidharreddy/.rbenv/shims/wkhtmltopdf'
-    # wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"
-end
+ }
+
+# if Rails.env.production?
+#     # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+#     wkhtmltopdf_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+# else
+#     wkhtmltopdf_path = '/Users/p.mahidharreddy/.rbenv/shims/wkhtmltopdf'
+#     # wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"
+# end
 
 WickedPdf.config = { exe_path: wkhtmltopdf_path, javascript_delay: 2000, wkhtmltopdf: wkhtmltopdf_path }
