4487299ac5bfbb9adf814c229203353c868b453c
Mahidhar P <mahidharreddy10@gmail.com>
2016-12-03 21:05:19 +0530

    gem spec

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -3,7 +3,7 @@
 # }
 
 if Rails.env.production?
-    wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+    wkhtmltopdf_path = '/home/deploy/production/echoworks/shared/bundle/ruby/2.3.0/gems/wkhtmltopdf-binary-0.12.3.1/bin/wkhtmltopdf_linux_amd64'
     # wkhtmltopdf_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
 else
     wkhtmltopdf_path = '/usr/local/bin/wkhtmltopdf'
