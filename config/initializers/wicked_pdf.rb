b92bf79bca454243bf46e862d6ca84132ee21e24
Mahidhar P <mahidharreddy10@gmail.com>
2016-12-03 20:53:37 +0530

    initialix

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-    @wkhtmltopdf_path = "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary-edge'].version}/bin/wkhtmltopdf_linux_x64"
+    @wkhtmltopdf_path = "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-edge-0.12.3.0/bin/wkhtmltopdf"
 else
     @wkhtmltopdf_path = `which wkhtmltopdf`.strip
 end
